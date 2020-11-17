66f74d9d2d5d1feff141bb0a77a3a096f8affc93
ShivanshGupta26 <73892374+ShivanshGupta26@users.noreply.github.com>
2020-11-17 22:01:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,51 +1,27 @@
-var car1,car2,car3,car4;
-var wall1,wall2,wall3;
+var car;
+var wall;
 var speed,weight;
 
 function setup() { 
   
   createCanvas(1600,400);
   
-  createSprite(70, 50, 20, 40);
-  createSprite(70,150,20,40);
-  createSprite(70,250,20,40);
-  createSprite(70,350,20,40);
-  
-  car1=createSprite(100,50,40,20);
-  car1.shapeColor=("red");
-  //car1.velocityX=speed;
-  
-  car2=createSprite(100,150,40,20);
-  car2.shapeColor=("green");
-  //car2.velocityX=speed;
-
-  car3=createSprite(100,250,40,20);
-  car3.shapeColor=("blue");
-  //car3.velocityX=speed;
-
-  car4=createSprite(100,350,40,20);
-  car4.shapeColor=("yellow");
-  //car4.velocityX=spped;
-
-  wall1=createSprite(800,100,1600,2);
-  wall1.shapeColor=("white");
-  
-  wall2=createSprite(800,200,1600,2);
-  wall2.shapeColor=("white");
-  
-  wall3=createSprite(800,300,1600,2);
-  wall3.shapeColor=("white");
-
   speed=random(55,90);
   weight=random(400,1500);
   
-  deformation=0.5*weight*speed*speed/22500;
+  car=createSprite(100,200,50,50);
+  car.shapeColor=("white");
+  car.velocityX=speed;
+
+  wall=createSprite(1200, 200, 60, 150);
+  wall.shapeColor=("grey");
+  
 }
 
 function draw() {
   background(0);  
-
-  if(wall1.x-car1.x < (car.width+wall1.width)/2){
+     
+  if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX=0;
     var deformation=0.5 * weight * speed *speed/22500;
     if(deformation>180){
